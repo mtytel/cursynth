a74557f11e602a8c6729165ef9aa3deb2733f5ea
iyoko <littleioaudio@gmail.com>
2013-12-23 17:35:52 -0800

    Update mopo.

--- mopo/src/tick_router.h
+++ mopo/src/tick_router.h
@@ -22,52 +22,14 @@
 
 namespace mopo {
 
-  class Feedback;
-
-  template<class P>
-  class TickRouter : public Router<P> {
+  class TickRouter : public ProcessorRouter {
     public:
-      virtual Processor* clone() const { return new TickRouter(*this); }
-      virtual void process();
+      TickRouter(int num_inputs = 0, int num_outputs = 0) :
+          ProcessorRouter(num_inputs, num_outputs) { }
+
+      virtual void process() = 0;
+      virtual void tick(int i) = 0;
   };
-
-  template<class P>
-  void TickRouter<P>::process() {
-    size_t num_processors = order_->size();
-    std::vector<P*> process_list;
-    process_list.reserve(num_processors);
-
-    for (size_t p = 0; p < num_processors; ++p) {
-      const Processor* next = order_->at(i);
-      if (processors_.find(next) == processors_.end())
-        processors_[next] = next->clone();
-
-      process_list[p] = processors_[next];
-    }
-
-    size_t num_feedbacks = feedbacks_.size();
-    for (size_t i = 0; i < num_feedbacks; ++i)
-      feedbacks_[i]->tickBeginRefreshOutput();
-
-    for (int i = 0; i < BUFFER_SIZE - 1; ++i) {
-      for (size_t p = 0; p < num_processors; ++p)
-        process_list[p]->tick(i);
-
-      for (size_t f = 0; f < num_feedbacks; ++f)
-        feedbacks_[f]->tick(i);
-
-      for (size_t f = 0; f < num_feedbacks; ++f)
-        feedbacks_[f]->tickRefreshOutput(i);
-    }
-
-    for (size_t p = 0; p < num_processors; ++p)
-      process_list[p]->tick(BUFFER_SIZE - 1);
-
-    for (size_t f = 0; f < num_feedbacks; ++f)
-      feedbacks_[f]->tick(BUFFER_SIZE - 1);
-
-    MOPO_ASSERT(num_processors != 0);
-  }
 } // namespace mopo
 
-#endif // TICK_ROUTER_H
+#endif // PROCESSOR_ROUTER_H
